e77dea3396dca805a529e48feb7d53f0e97888e1
daniel <danielsoledad@gmail.com>
2021-08-01 03:48:51 -0700

    Adding Navigation Stack to project

---
+++ screens/MapScreen/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text>
+                This is My Map Screen
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default MapScreen;
